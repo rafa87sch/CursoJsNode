4e6610ede5e78609c03b669402b07ac12ceee896
Rafa Soto <rsotocha@cr.ibm.com>
2019-04-07 11:42:37 -0600

    sect3-13

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,14 @@
+const valid = require('validator');
 const utils=require('./utils.js');
 const notes = require('./notes.js');
+const chalk = require('chalk');
 const sum = utils(1,2);
 
 console.log(sum);
 console.log(notes());
 
+console.log(chalk.green('rafa'));
+console.log(chalk.blue('rafa'));
+
+console.log(valid.isEmail('example.com'));
+console.log(valid.isURL('www.google.com'));
