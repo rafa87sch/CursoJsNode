974bd42cae5dc3a4afd53f64357fd1a4ceb372c6
Rafa <rafa87sch@hotmail.com>
2019-04-22 17:17:11 -0600

    yargs files

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,12 +3,29 @@ const utils=require('./utils.js');
 const notes = require('./notes.js');
 const chalk = require('chalk');
 const sum = utils(1,2);
+const yargs =require('yargs');
 
-console.log(sum);
-console.log(notes());
 
-console.log(chalk.green('rafa'));
-console.log(chalk.blue('rafa'));
+//yargs
+console.log("Yargs");
+console.log(yargs.argv);
+const command = process.argv[2];
 
-console.log(valid.isEmail('example.com'));
-console.log(valid.isURL('www.google.com'));
+if(command==='add'){
+    console.log("you are adding a note");
+}else if(command==='remove'){
+    console.log("you are remove a note");
+}else if(command==='read'){
+    console.log("you are reading a note");
+}
+//set up the version
+yargs.version('1.1.0');
+
+//add and remove with yard
+yargs.command({
+    command : 'add',
+    describe: 'add a new note',
+    handler: function(){
+        console.log("yargs add note")
+    }
+})
