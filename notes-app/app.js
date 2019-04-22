f7a5f5fcac5faf6fa716d9bfd573c2a5d9c9f0c0
Rafa <rafa87sch@hotmail.com>
2019-04-22 17:35:06 -0600

    notes add function read, list

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,23 +1,7 @@
 const valid = require('validator');
-const utils=require('./utils.js');
-const notes = require('./notes.js');
 const chalk = require('chalk');
-const sum = utils(1,2);
 const yargs =require('yargs');
 
-
-//yargs
-console.log("Yargs");
-console.log(yargs.argv);
-const command = process.argv[2];
-
-if(command==='add'){
-    console.log("you are adding a note");
-}else if(command==='remove'){
-    console.log("you are remove a note");
-}else if(command==='read'){
-    console.log("you are reading a note");
-}
 //set up the version
 yargs.version('1.1.0');
 
@@ -28,4 +12,30 @@ yargs.command({
     handler: function(){
         console.log("yargs add note")
     }
-})
+});
+
+yargs.command({
+    command : 'remove',
+    describe: 'remove a new note',
+    handler: function(){
+        console.log("remove add note")
+    }
+});
+
+yargs.command({
+    command : 'read',
+    describe: 'read a new note',
+    handler: function(){
+        console.log("yargs read note")
+    }
+});
+
+yargs.command({
+    command : 'list',
+    describe: 'list a note',
+    handler: function(){
+        console.log("list  note")
+    }
+});
+
+console.log(yargs.argv);
